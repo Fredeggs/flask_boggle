bc75d4b589438be36a14f91962fdf36929cdf11c
Fredeggs <ian.dauphin@gmail.com>
2022-05-26 16:46:49 -0400

    added score functionality and basic css

--- static/app.js
+++ static/app.js
@@ -1,7 +1,10 @@
 "use strict";
 
+
 const $boggleForm = $("#guess-word");
 const $guessMessage = $("#guess-message");
+const $score = $('#score');
+
 $boggleForm.on("submit", guessWord);
 
 async function guessWord(evt) {
@@ -14,10 +17,36 @@ async function guessWord(evt) {
     data: { $guess },
   });
   if (response.data.result === "ok") {
-    $guessMessage.text("Good Guess!");
+    let score = Number($score.text())
+    score += Number($guess.length)
+    $score.text(score)
+    if ($guessMessage.css('display') === 'none') {
+      $guessMessage.css('display', 'block');
+      $guessMessage.text("Good Guess!").fadeOut(3000);
+    }
+    else {
+      $guessMessage.text("Good Guess!").fadeOut(3000);
+    }
   } else if (response.data.result === "not-on-board") {
-    $guessMessage.text("That word is not on the board");
+    if ($guessMessage.css('display') === 'none') {
+      $guessMessage.css('display', 'block');
+      $guessMessage.text("That word is not on the board").fadeOut(3000);
+    }
+    else {
+      $guessMessage.text("That word is not on the board").fadeOut(3000);
+    }
   } else {
-    $guessMessage.text("That is not a word");
+    if ($guessMessage.css('display') === 'none') {
+      $guessMessage.css('display', 'block');
+      $guessMessage.text("That is not a word").fadeOut(3000);
+    }
+    else {
+      $guessMessage.text("That is not a word").fadeOut(3000);
+    }
   }
+}
+
+function countDown() {
+  setTimeout() // Disable the ability to submit guesses after 60 sec
+  setInterval() // Subtract 1 from the timer after each second
 }
